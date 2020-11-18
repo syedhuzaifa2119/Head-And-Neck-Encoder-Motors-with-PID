8fa0f01ee97c759850ec9225a8141007a4f812d7
syedhuzaifa2119 <72070685+syedhuzaifa2119@users.noreply.github.com>
2020-11-18 17:02:57 +0500

    Reading serial Input

--- reding_from_encoder.ino
+++ reding_from_encoder.ino
@@ -4,6 +4,9 @@ int encoder0PinA = 2;
 int encoder0PinB = 3;
 int encoder0Pos = 0;
 int encoder0PinALast = LOW;
+float speedvalue=0;
+int anglevalue=0;
+int count;
 int n = LOW;
 
 void setup() {
@@ -14,8 +17,17 @@ void setup() {
 }
 
 void loop() {
- Serial.print (encoder0Pos);
-    Serial.print ("\n");
+
+  //Serial.print (encoder0Pos);
+ //Serial.print ("\n");
+     if (Serial.available()>0)
+    {
+      Serial.print("Enter the Rotation Angle ");
+      anglevalue=Serial.parseInt();
+      Serial.print("The Angle is ");
+      Serial.println(anglevalue);
+      }
+    
 
 }
 void encoder ()
@@ -23,10 +35,12 @@ void encoder ()
     n = digitalRead(encoder0PinA);
   if ((encoder0PinALast == LOW) && (n == HIGH)) {
     if (digitalRead(encoder0PinB) == LOW) {
-      encoder0Pos--;
-    } else {
       encoder0Pos++;
+    } else {
+      encoder0Pos--;//CCW
     }
+} 
+
 }
- 
-}
+
+
