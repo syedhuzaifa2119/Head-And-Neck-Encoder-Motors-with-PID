a1b96a7b32d27cf409e7ae3bdf47d3857601a6c5
syedhuzaifa2119 <72070685+syedhuzaifa2119@users.noreply.github.com>
2020-11-19 15:07:57 +0500

    1 Motor control with encoder CW

--- reding_from_encoder.ino
+++ reding_from_encoder.ino
@@ -3,6 +3,8 @@ int encoder0PinA = 2;
 int encoder0PinB = 3;
 int encoder0Pos = 0;
 int encoder0PinALast = LOW;
+int in1=6;
+int in2=7;
 float speedvalue=0;
 float anglevalue=0;
 int count;
@@ -13,26 +15,36 @@ void setup() {
     Serial.begin (9600);
   pinMode (encoder0PinA, INPUT);
   pinMode (encoder0PinB, INPUT);
+  pinMode (in1,INPUT);
+  pinMode (in2,INPUT);
   attachInterrupt(digitalPinToInterrupt(2), encoder, CHANGE);
 }
 
 void loop() {
 
-  //Serial.print (encoder0Pos);
- //Serial.print ("\n");
+  Serial.print (encoder0Pos);
+ Serial.print ("\n");
      if (Serial.available()>0)
     {
+      anglevalue=0;
+   
       Serial.print("Enter the Rotation Angle ");
       anglevalue=Serial.parseInt();
       Serial.print("The Angle is ");
       Serial.println(anglevalue);
-      encpulses=anglevalue*(1600.0/360.0);
+      encpulses=(anglevalue*(1600.0/360.0))-100;
       Serial.print("No. of encoder pulses");
       Serial.println(encpulses);
       
       }
-      if(
-    
+       motorClockwise(encpulses);
+//      if(encpulses>0){
+//         motorClockwise(encpulses);
+//       }
+//        if(encpulses<0){
+//          motorCounterClockwise(encpulses);
+//          }
+//       
 
 }
 void encoder ()
@@ -41,12 +53,31 @@ void encoder ()
   if ((encoder0PinALast == LOW) && (n == HIGH)) {
     if (digitalRead(encoder0PinB) == LOW) {
       encoder0Pos++;
-      if(encoder0Pos<=encpulses)
     } else {
       encoder0Pos--;//CCW
     }
 } 
 
 }
-
-
+void motorClockwise(int pulses)
+{
+  while (pulses>encoder0Pos){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  if(pulses<encoder0Pos){break;}
+  }
+ 
+    digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  }
+// void motorCounterClockwise(int pulses)
+// {
+//  while (pulses<encoder0Pos){
+//  digitalWrite(in1,LOW);
+//  digitalWrite(in2,HIGH);
+//  if(pulses>encoder0Pos){break;}
+//  }
+// 
+//    digitalWrite(in1,LOW);
+//  digitalWrite(in2,LOW);
+//  }
